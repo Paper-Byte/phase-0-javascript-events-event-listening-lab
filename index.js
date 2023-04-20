c90be0b8ea6b331c63be1b9db501139238a7d657
Noah <noahhughesph@gmail.com>
2023-04-19 21:20:29 -0400

    test passing

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+
+
+const clickAlert = () => {
+    alert('I was clicked!');
+}
+const input = document.getElementById('button');
+input.addEventListener('click', clickAlert);
 }
